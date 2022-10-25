d10808adc573cd95973c9df4532cf5947cf96271
Tuan Nguyen Bao <minecraftqn123@gmail.com>
2022-10-25 08:42:59 +0700

    Mono update

--- MainFract.cpp
+++ MainFract.cpp
@@ -1,7 +1,6 @@
 #include "Fract.h"
-
-int main()
-{
+#include "Mono.h"
+void fractProcess() {
     Fract f, g;
     cout << "Luu y: Mau so phai khac 0, neu khong mau so = 1\n";
     cout << "Nhap phan so dau tien: ";
@@ -13,5 +12,27 @@ int main()
     cout << f << " - " << g << " = " << f - g << '\n';
     cout << f << " * " << g << " = " << f * g << '\n';
     cout << f << " / " << g << " = " << f / g << '\n';
+}
+void monoProcess() {
+    Mono f, g;
+    cout << "Nhap don thuc thu nhat: ";
+    cin >> f;
+    cout << "Nhap don thuc thu hai: ";
+    cin >> g;
+    cout << "Cac phep tinh tren don thuc\n";
+    cout << f << " + " << g << " = " << f + g << '\n';
+    cout << f << " - " << g << " = " << f - g << '\n';
+    cout << f << " * " << g << " = " << f * g << '\n';
+    cout << f << " / " << g << " = " << f / g << '\n';
+    cout << "Dao ham cua don thuc thu nhat : " << f.derivative() << '\n';
+    cout << "Dao ham cua don thuc thu hai : " << g.derivative() << '\n';
+    cout << "Nguyen ham cua don thuc thu nhat: ";
+    f.anti_derivative();
+    cout << "Nguyen ham cua don thuc thu hai: ";
+    g.anti_derivative();
+}
+int main()
+{
+    monoProcess();
     return 0;
 }
--- Mono.h
+++ Mono.h
@@ -1,5 +1,26 @@
 #pragma once
+#ifndef _MONO_H_
+#define _MONO_H_
+#include <iostream>
+#include <math.h>
+using namespace std;
 class Mono
 {
+	double coef, expo;
+public:
+	void setCoef(double);
+	void setExpo(double);
+	double getCoef();
+	double getExpo();
+	double computeValue(double);
+	Mono derivative();
+	void anti_derivative();
+	Mono operator+(const Mono&);
+	Mono operator-(const Mono&);
+	Mono operator*(const Mono&);
+	Mono operator/(const Mono&);
+	friend istream& operator>>(istream&, Mono&);
+	friend ostream& operator<<(ostream&, const Mono&);
 };
+#endif // !
 
