7fd6bb54d9a39f68868ce8a5a827d5f3afdf620e
Ngoc <dungtuyetngoc@gmail.com>
2022-10-25 08:05:28 +0700

    worked on Complex class

--- Complex.cpp
+++ Complex.cpp
@@ -1,1 +1,51 @@
 #include "Complex.h"
+
+int Complex::getReal()
+{
+	return real; 
+}
+
+int Complex::getImg()
+{
+	return img;
+}
+
+void Complex::setReal()
+{
+	int r; 
+	cout << "Type real part: "; cin >> r;
+	real = r;
+}
+
+void Complex::setImg()
+{
+	int img;
+	cout << "Type real part: "; cin >> img;
+	this->img = img; 
+}
+
+Complex operator+(Complex& a, Complex& b)
+{
+	int img = a.getImg() + b.getImg(); 
+	int real = a.getReal() + b.getReal(); 
+	return Complex(real, img); 
+}
+
+Complex operator-(Complex& a, Complex& b)
+{
+	int img = a.getImg() - b.getImg();
+	int real = a.getReal() - b.getReal();
+	return Complex(real, img);
+}
+
+Complex operator*(Complex& a, Complex& b)
+{
+	int img = a.getReal() * b.getImg() + a.getImg() * b.getReal();
+	int real = a.getReal() * b.getReal() - a.getImg() * b.getImg();
+	return Complex(real, img);
+}
+
+Complex operator/(Complex& a, Complex& b)
+{
+	
+}
--- Complex.h
+++ Complex.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include <iostream>
+using namespace std; 
+
+
 class Complex
 {
+	int real, img;
+public: 
+	Complex():real(0), img(0){}
+	Complex(int r, int i):real(r), img(i){}
+	int getReal(); 
+	int getImg(); 
+	void setReal(); 
+	void setImg(); 
+	//friend Complex operator+(Complex&, Complex&); 
 };
 
